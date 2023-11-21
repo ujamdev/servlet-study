cb229a576da27dd57b5edb254a979b0f3d836df8
ujamdev <ujamdev@gmail.com>
2023-11-21 23:31:21 +0900

    feat: @RequestMapping 기반의 회원 목록 컨트롤러 구현

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Map;
-
 @Controller
 public class SpringMemberSaveControllerV1 {
 
